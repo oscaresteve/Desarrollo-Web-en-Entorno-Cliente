4eec4b46e486bb9b118fe13f5ebfeb6d29a2956c
oscaresteve <oscarestevea24@gmail.com>
2025-11-07 12:47:37 +0100

    Acabado

--- Actividades_JavaScript/src/convertir_a_funcional/index.js
+++ Actividades_JavaScript/src/convertir_a_funcional/index.js
@@ -19,14 +19,10 @@ export {
   markFavorites,
 };
 
-let alerts = [];
-
 const getAlerts = async () => {
   const url = "https://data.food.gov.uk/food-alerts/id?_limit=100";
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.items);
-
   return data.items;
 };
 
@@ -39,15 +35,15 @@ const mapFavorites = (favorites) => (alerts) => {
 
 const toggleToFavorites = (favorites) => (alertId) => {
   const newFavorites = new Set(favorites);
-  favorites.has(alertId)
-    ? newFavorites.delete(alertId)
-    : newFavorites.add(alertId);
+  (favorites.has(alertId) && newFavorites.delete(alertId)) ||
+    newFavorites.add(alertId);
   return newFavorites;
 };
 
 const createAlertDiv = (alert) => {
   const divAlert = document.createElement("div");
   divAlert.className = "#DIV";
+  divAlert.dataset.id = alert["@id"];
 
   const title = document.createElement("h2");
   title.textContent = alert.title;
@@ -86,63 +82,29 @@ const markFavorites = (favorites) => (divsFavorites) => {
   divsFavorites.forEach((div) => {
     if (favorites.has(div.dataset.id)) {
       div.classList.add("favorita");
+    } else {
+      div.classList.remove("favorita");
     }
   });
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  
+document.addEventListener("DOMContentLoaded", async () => {
+  let alerts = await getAlerts();
+  let favorites = new Set([]);
+  const alertsWithFav = mapFavorites(favorites)(alerts);
+  const divsFavorites = createAlertsDivs(alertsWithFav);
+
+  let clickableDivs;
+
+  const handler = (ev) => {
+    const id = ev.currentTarget.dataset.id;
+    favorites = toggleToFavorites(favorites)(id);
+    markFavorites(favorites)(clickableDivs);
+    console.log(favorites);
+
+    console.log(clickableDivs);
+  };
+
+  clickableDivs = addClickListeners(divsFavorites, handler);
+  document.getElementById("container").append(...clickableDivs);
 });
-
-/*
-let alertasFavoritas = [];
-
-function addFavorita() {
-  alertasFavoritas.push(this);
-  console.log(alertasFavoritas);
-  let alertasDivs = document.querySelectorAll(".alerta");
-  alertasDivs.forEach(function (a) {
-    a.classList.remove("favorita");
-    alertasFavoritas.forEach(function (fav) {
-      if (a === fav) {
-        a.classList.add("favorita");
-      }
-    });
-  });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://data.food.gov.uk/food-alerts/id?_limit=100").then((response) =>
-    response.json().then((data) => {
-      const alertes = data.items;
-      for (let alerta of alertes) {
-        const divAlerta = document.createElement("div");
-        divAlerta.classList.add("alerta");
-        divAlerta.innerHTML = `
-                <h2>${alerta.title}</h2>
-                <h3>${alerta.created}</h3>
-                <h4>Problems:</h4>
-                <ul class="problems"></ul>
-                <h4>Details:</h4>
-                <ul class="details"></ul>
-                `;
-        for (let p of alerta.problem) {
-          const liProblem = document.createElement("li");
-          liProblem.textContent = p.riskStatement;
-          divAlerta.querySelector("ul.problems").append(liProblem);
-        }
-        if (alerta.productDetails) {
-          for (let p of alerta.productDetails) {
-            const liDetail = document.createElement("li");
-            liDetail.textContent = p.productName;
-            divAlerta.querySelector("ul.details").append(liDetail);
-          }
-        }
-
-        document.querySelector("#container").append(divAlerta);
-        divAlerta.addEventListener("click", addFavorita);
-      }
-    })
-  );
-});
-*/
